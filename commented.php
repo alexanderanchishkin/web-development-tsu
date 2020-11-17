45624f9f1c45f9a410b538ce4185bfa09710e0c2
Alexander Anchishkin <alexanderanchishkin@gmail.com>
2020-11-17 14:38:51 +0300

    Вывели комментарии в отдельный файл

---
+++ commented.php
@@ -0,0 +1,24 @@
+<?php
+	// include
+	// include_once
+	// require
+	// require_once
+	//
+	//
+	//
+	//$image_paths = ['/images/img1.png', '/images/img2.png', '/images/img3.png'];
+
+	/* Проверяли работу scandir()
+	echo "<pre>";
+	var_dump(scandir("./images"));
+	echo "</pre>"; */
+
+	// images.php -> images_utilities.php -> strings_utilities.php
+	//
+	//
+	//
+		// <img src="/images/img1.png" />
+		//echo '<img src="' . $image_path . '" />';
+
+		//echo "<img src='$image_path' />";
+		//echo "<img src=\"$image_path\" />";
--- images.php
+++ images.php
@@ -9,51 +9,30 @@
 <code><?=htmlspecialchars('<?php ?>')?></code>
 
 <?php
-	// include
-	// include_once
-	// require
-	// require_once
+
 	require_once "utilities/images_utilities.php";
 
-	//$image_paths = ['/images/img1.png', '/images/img2.png', '/images/img3.png'];
 
-	/* Проверяли работу scandir()
-	echo "<pre>";
-	var_dump(scandir("./images"));
-	echo "</pre>"; */
+
+	function print_images($image_directory)
+	{
+
+	}
 
 	$image_filenames = scandir("./images");
 
-	// images.php -> images_utilities.php -> strings_utilities.php
 
 	foreach ($image_filenames as $image_filename)
 	{
-		//TODO: Проверить, является ли картинкой
-
-		//Вывести картинку
-
 		if (!is_image($image_filename))
 			continue;
 
 		$arr = ["img" => $image_filename];
 		$image_path = "/images/{$arr['img']}";
-
-		// <img src="/images/img1.png" />
-		//echo '<img src="' . $image_path . '" />';
-
-		//echo "<img src='$image_path' />";
-		//echo "<img src=\"$image_path\" />";
-
-
 ?>
-
 		<img src="<?=$image_path?>" />
-
-
 <?php
-
 	}
-
 ?>
 
 </body>
