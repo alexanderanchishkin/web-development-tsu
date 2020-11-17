b44a691d0282eeb944bcfa491249dd01bfc23edc
Alexander Anchishkin <alexanderanchishkin@gmail.com>
2020-11-17 12:00:05 +0300

    Update echo.php

--- echo.php
+++ echo.php
@@ -1,2 +1,22 @@
 <?php
-	echo $_POST['name'] . ' ' . $_POST['lastName'];
+/*
+	$b = $_GET['a'] . "<br>";
+	echo $b;
+	$a = 1;
+	$b = 'Hello $a World! ' . $a;
+	$c = '1';
+	if ($a == $c)
+	{
+
+	}
+	else
+	{
+
+	}
+*/
+
+	$name = $_GET['name'];
+	$lastname = $_GET['lastname'];
+
+	echo "Вас зовут $name $lastname!";
+	//echo $_POST['name'] . ' ' . $_POST['lastName'];
