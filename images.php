eab2fc3528d0ae2704176df3411ceeff96235862
Alexander Anchishkin <alexanderanchishkin@gmail.com>
2020-11-17 17:12:46 +0300

    Добавили форму загрузки изображений

--- images.php
+++ images.php
@@ -6,6 +6,12 @@
 </head>
 <body>
 
+<form enctype="multipart/form-data" method="POST" action="./load_image.php">
+	<input type="file" name="filename"><br>
+	<input type="submit" value="Отправить изображение">
+	<input type="hidden" name="description" value="Загрузка изображения">
+</form>
+
 <?php
 	require_once "utilities/images_utilities.php";
 	print_images('./images/');
