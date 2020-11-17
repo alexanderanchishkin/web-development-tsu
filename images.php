b338fc44f25a72299d057603bba21d78aa430a46
Alexander Anchishkin <alexanderanchishkin@gmail.com>
2020-11-17 14:52:15 +0300

    Разделили функции вывода изображений

--- images.php
+++ images.php
@@ -10,29 +10,30 @@
 
 <?php
 
+	define("ROOT", $_SERVER['DOCUMENT_ROOT']);
+
 	require_once "utilities/images_utilities.php";
 
+	function print_image($image_directory, $image_filename)
+	{
+		if (!is_image($image_filename))
+			return;
 
+		$image_path = "$image_directory/$image_filename";
+
+		echo "<img src=\"$image_path\" />";
+	}
 
 	function print_images($image_directory)
 	{
+		$image_filenames = scandir($image_directory);
 
+		foreach ($image_filenames as $image_filename)
+			print_image($image_directory, $image_filename);
 	}
 
-	$image_filenames = scandir("./images");
 
-
-	foreach ($image_filenames as $image_filename)
-	{
-		if (!is_image($image_filename))
-			continue;
-
-		$arr = ["img" => $image_filename];
-		$image_path = "/images/{$arr['img']}";
-?>
-		<img src="<?=$image_path?>" />
-<?php
-	}
+	print_images('./images/');
 ?>
 
 </body>
