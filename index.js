6fea798ea34415410d118c44ad3c21c540b06c4c
ViktorSuver <suvernet@yande.ru>
2020-11-29 00:26:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -58,11 +58,11 @@ function sayHello()
 	console.log("Привет из Белгорода!");
 	console.log("Привет от Ленара");
 	console.log("Привет от Un-R");
-    console.log("Привет от Виктора");
+        console.log("Привет от Виктора");
 	console.log("Привет от Un-R");
 	console.log("Привет от Иры из Калининграда!)");
 	console.log("Здесь был я");
-    console.log("и я!");
+        console.log("и я!");
 	console.log("Привет от albert-garipov");
 	console.log("Привет от EvgenyShevelev");
 	console.log("Hello from Navka");
@@ -75,7 +75,7 @@ function sayHello()
 	console.log("Привет от Марины!");
 	console.log("Привет от MrG-Vladimir");
 	console.log("Привет от Александра");
-    console.log("Привет от Наталии");
+        console.log("Привет от Наталии");
 }
 
 function main()
